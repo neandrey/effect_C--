342e8f902a002abda5eaa4ee1f979297ae0504ab
ne_andrey <sorokinac@gmail.com>
2023-05-23 23:23:25 +0500

    correct_str_2

--- Ch_02/exercise/02_correct_str2/correct.cpp
+++ Ch_02/exercise/02_correct_str2/correct.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main() {
 
     const string exclam = "!";
-    const string message = "Привет" + ", мир" + exclam; // недопустима, потомучто два символьных литерала.
+    //const string message = "Привет" + ", мир" + exclam; - недопустима, потомучто два символьных литерала.
+    const string message = "Привет, мир" + exclam;
 
     cout << exclam << endl
          << message << endl;
