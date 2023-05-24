204c97c571ab7912840e63d8d7863e52f672aaaa
ne_andrey <sorokinac@gmail.com>
2023-05-24 14:19:40 +0500

    cont_numb

---
+++ Ch_03/exercise/07_count_10_to_5/count.cpp
@@ -0,0 +1,13 @@
+// Программа которая считает от 10 до -5
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 10; i != -5; --i)
+        cout << i << ' ';
+    cout << endl;
+
+    return 0;
+}
