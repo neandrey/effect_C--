2e55a8cf963b7bc546852a0a7632b82db068e7c0
ne_andrey <sorokinac@gmail.com>
2023-05-24 14:14:13 +0500

    what_code

---
+++ Ch_03/exercise/06_what_code/what_code.cpp
@@ -0,0 +1,14 @@
+// Что делает следующий код?
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int i = 0;
+    while (i < 10) {
+        i += 1;
+        cout << i << endl;
+    }
+    return 0;    
+}
