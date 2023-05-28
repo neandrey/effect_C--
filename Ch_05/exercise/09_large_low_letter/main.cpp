9903b1e059a7de16757563b4b2aea802aeff19f7
ne_andrey <sorokinac@gmail.com>
2023-05-29 03:14:03 +0500

    low_upper

--- Ch_05/exercise/09_large_low_letter/main.cpp
+++ Ch_05/exercise/09_large_low_letter/main.cpp
@@ -1,24 +1,31 @@
 // Напишите программу для вывода слов (из введенных даннх)
 // строчными буквами, а после них - прописными буквами.
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <cctype>
 #include "split.h"
 #include "lower_upper.h"
+#include <stdexcept>
 
 
-using std::string;  using std::vector;
-using std::getline; using std::cin;
-using std::cout;    using std::endl;
-using std::tolower; using std::toupper;
+using std::string;      using std::vector;
+using std::getline;     using std::cin;
+using std::cout;        using std::endl;
+using std::tolower;     using std::toupper;
+using std::ifstream;    using std::runtime_error;  
 
 
 int main() {
 
     vector<string> v_str;
+    string in_file = "low_up.txt";
+    ifstream ist {in_file};
+    if (!ist) throw runtime_error("Невозможно открыть входной файл");
+
     // Ввод строк из потока.
-    for (string s; getline(cin, s); )
+    for (string s; getline(ist, s); )
     {
         v_str = split(s);
     }
