e324fe5984530a03420366deccf2f213e6e75d9b
ne_andrey <sorokinac@gmail.com>
2023-05-30 15:10:47 +0500

    my_reverse

---
+++ Ch_08/chapter/05_my_reverse/main.cpp
@@ -0,0 +1,35 @@
+// find - последовательный доступ к элементу
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+
+using std::vector;  using std::cout;
+using std::endl;    using std::string;
+using std::back_inserter;
+
+namespace My_name{
+template <class Bi> 
+void reverse(Bi beg, Bi end)
+{
+    while (beg != end){
+        --end;
+        if(beg != end)
+            std::swap(*beg++, *end);
+    }
+        
+}
+}
+
+int main() {
+    
+    vector<int> v_int {5, 3, 2, 1, 4};
+    My_name::reverse(v_int.begin(), v_int.end()); 
+
+    for (auto i : v_int)
+        cout << i << ' ';
+    cout << endl;
+
+    
+    return 0;
+}
