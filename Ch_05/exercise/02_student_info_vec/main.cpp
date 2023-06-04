f56f3eb76e2f213eda7fdf83aead6aeb83e98c17
Andrey Sorokin <sorokinac@gmail.com>
2023-06-04 10:24:59 +0500

    std_vec

--- Ch_05/exercise/02_student_info_vec/main.cpp
+++ Ch_05/exercise/02_student_info_vec/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <algorithm>
 #include <iomanip>
 #include <ios>
@@ -14,6 +15,12 @@ using std::cout;            using std::sort;
 using std::domain_error;    using std::streamsize;
 using std::endl;            using std::string;
 using std::max;             using std::vector;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::microseconds;
+
+
 
 int main() {
     
@@ -36,9 +43,18 @@ int main() {
     // Упорядочиваем записи с данными о студентах по алфавиту.
     sort(students.begin(), students.end(), compare);
 
+    //для замера производительности
+    auto start = high_resolution_clock::now();
+
     // Отделяем cдавших студентов от не cдавших
     vector<Student_info> fails = extract_fails(students);
 
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Время работы extract_fails "
+         << duration.count() << " microseconds." << endl;
     
     for (vector<Student_info>::size_type i = 0; i != fails.size(); ++i)
         cout << fails[i].name << " " << grade(fails[i]) << endl;
