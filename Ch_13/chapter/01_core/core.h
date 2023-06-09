a8c57ab7fefb6095c061a6f6c9e42c0a6530799e
ne_andrey <sorokinac@gmail.com>
2023-06-09 16:31:38 +0500

    inheritance

---
+++ Ch_13/chapter/01_core/core.h
@@ -0,0 +1,38 @@
+#ifndef CORE_H
+#define CORE_H
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Core {
+public:
+    Core(): midterm{0}, final{0}
+    {}
+    Core(std::istream& is) { read(is); }
+    std::string name() const;
+    virtual std::istream& read(std::istream&);
+    virtual double grade() const;
+protected:
+    std::istream& read_common(std::istream&);
+    double midterm, final;
+    std::vector<double> homework;
+private:
+    std::string n;
+};
+
+class Grade: public Core {
+public:
+    Grade(): thesis{0}
+    {}
+    Grade(std::istream& is) { read(is); }
+    double grade() const override;
+    std::istream& read(std::istream&) override;
+private:
+    double thesis;
+};
+
+
+std::istream& read_hw(std::istream& in, std::vector<double>& hw);
+bool compare(const Core& c1, const Core& c2);
+bool compare_grades(const Core& c1, const Core& c2);
+#endif
